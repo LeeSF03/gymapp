146962c2612521ba9b8327a53ddf6722b914ca2a
LeeSF03 <shuenfeilee@gmail.com>
2025-10-06 17:25:32 +0800

    feat(auth): add otp page

---
+++ components/otp-input.tsx
@@ -0,0 +1,63 @@
+import { TextInput, View } from "react-native"
+import { Input } from "./ui/input"
+import { cn } from "@/lib/utils"
+import { useRef, useState } from "react"
+
+type OtpInputProps = {
+  otp: string
+  handleOtpChange: (otp: string) => void
+  length?: number
+}
+
+function OtpInput({ otp, handleOtpChange, length = 6 }: OtpInputProps) {
+  const [isFocused, setIsFocused] = useState(false)
+  const otpHiddenInput = useRef<TextInput>(null)
+
+  const onBlur = () => {
+    setIsFocused(false)
+  }
+  const onFocus = () => {
+    otpHiddenInput.current?.focus()
+    setIsFocused(true)
+  }
+
+  return (
+    <View>
+      {/* To hide text input without affecting layout */}
+      <View className="h-0 w-0">
+        <TextInput
+          onBlur={onBlur}
+          ref={(node) => {
+            if (node) otpHiddenInput.current = node
+          }}
+          onChangeText={(otpInput) => {
+            if (otp.length < length) handleOtpChange(otpInput)
+          }}
+          onKeyPress={({ nativeEvent }) => {
+            if (nativeEvent.key === "Backspace" && otp.length > 0) {
+              handleOtpChange(otp.slice(0, otp.length - 1))
+            }
+          }}
+          value={otp}
+          keyboardType="number-pad"
+        />
+      </View>
+      <View className="flex flex-row gap-x-2">
+        {Array.from({ length }).map((_, index) => (
+          <Input
+            className={cn("aspect-square flex-1 text-center", {
+              "border-primary":
+                (isFocused && otp.length === index) || otp.length === length,
+            })}
+            onFocus={onFocus}
+            value={otp[index] ?? ""}
+            key={index}
+            caretHidden
+          />
+        ))}
+      </View>
+    </View>
+  )
+}
+
+export { OtpInput, type OtpInputProps }
